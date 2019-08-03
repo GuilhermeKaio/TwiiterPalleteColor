aaae475dd5806c2a88f919e22555c836c3919845
Guilherme Kaio <guilherme_kaio@outlook.com>
2019-08-03 00:57:42 -0300

    Pallete

--- robots/image.js
+++ robots/image.js
@@ -2,6 +2,10 @@ const state = require('./state.js')
 const imageDownloader = require('image-downloader')
 const google = require('googleapis').google
 const customSearch = google.customsearch('v1')
+const { getPaletteFromURL } = require('color-thief-node')
+const rgbHex = require('rgb-hex')
+var fs = require('fs')
+  , gm = require('gm').subClass({imageMagick: true})
 
 const googleSearchCredentials = require('../credentials/google-search.json')
 
@@ -10,6 +14,8 @@ async function robots() {
   const content = state.load()
   await fetchImagesOfAllSentences(content)
   await downloadAllImages(content)
+  await imagePallete()
+  createColorImage(content)
 
   state.save(content)
 
@@ -46,7 +52,7 @@ async function robots() {
   async function downloadAllImages(content) {
     content.downloadedImages = []
     imageUrl = `${content.image}`
-    await downloadAndSave(imageUrl, `original.png`)
+    await downloadAndSave(imageUrl, `original.jpg`)
     try{
     content.downloadedImages.push(imageUrl)
     console.log(`> [image-robot] [${content.searchTerm}] Image successfully downloaded: ${imageUrl}`)
@@ -61,6 +67,79 @@ async function robots() {
       url: url,
       dest: `./content/${fileName}`
     })
+  }
+
+  async function imagePallete(){
+    fileName = `./content/original.jpg`
+    const colorPallete = await getPaletteFromURL(fileName);
+    color1 = colorPallete[0]
+    content.color1 = rgbHex(color1[0],color1[1],color1[2])
+    color2 = colorPallete[1]
+    content.color2 = rgbHex(color2[0],color2[1],color2[2])
+    color3 = colorPallete[2]
+    content.color3 = rgbHex(color3[0],color3[1],color3[2])
+    color4 = colorPallete[3]
+    content.color4 = rgbHex(color4[0],color4[1],color4[2])
+    color5 = colorPallete[4]
+    content.color5 = rgbHex(color5[0],color5[1],color5[2])
+  }
+
+  function createColorImage(content){
+    gm(100, 100,'#'+ content.color1)
+      .write('./content/color1.jpg', function (err) {
+      })
+    gm(100, 100,'#'+ content.color2)
+      .write('./content/color2.jpg', function (err) {
+      })
+    gm(100, 100,'#'+ content.color3)
+      .write('./content/color3.jpg', function (err) { 
+      })
+    gm(100, 100,'#'+ content.color4)
+      .write('./content/color4.jpg', function (err) {
+      })
+    gm(100, 100,"#"+ content.color5)
+      .write('./content/color5.jpg', function (err) {
+      })
+    
+    gm(900, 900,'#ddff99f3')
+      .write('./content/background.jpg', function (err) {
+      })
+    
+    gm('./content/background.jpg')
+      .composite('./content/color1.jpg')
+      .geometry('+40+0')
+      .write('./content/background.jpg', function(err) {
+        if(!err) console.log("Written montage image.");
+    })
+
+    gm('./content/background.jpg')
+      .composite('./content/color2.jpg')
+      .geometry('+220+0')
+      .write('./content/background.jpg', function(err) {
+        if(!err) console.log("Written montage image.");
+    })
+
+    gm('./content/background.jpg')
+      .composite('./content/color3.jpg')
+      .geometry('+400+0')
+      .write('./content/background.jpg', function(err) {
+        if(!err) console.log("Written montage image.");
+    })
+
+    gm('./content/background.jpg')
+      .composite('./content/color4.jpg')
+      .geometry('+580+0')
+      .write('./content/background.jpg', function(err) {
+        if(!err) console.log("Written montage image.");
+    })
+
+    gm('./content/background.jpg')
+      .composite('./content/color5.jpg')
+      .geometry('+760+0')
+      .write('./content/background.jpg', function(err) {
+        if(!err) console.log("Written montage image.");
+    })
+    
   }
 
 }
--- robots/input.js
+++ robots/input.js
@@ -4,20 +4,13 @@ const state = require('./state.js')
 function robot() {
   const content = {
 }
-  //const googleSearchCredentials = require('../credentials/google-search.json')
 
   content.searchTerm = askAndReturnSearchTerm()
-  //content.searchEngineId = askAndReturnSearchEngineId()
 
   state.save(content)
 
   function askAndReturnSearchTerm() {
     return readline.question('Type a Image search term: ')
-  }
-
-  function askAndReturnSearchEngineId(){
-
-      return googleSearchCredentials.searchEngineId
   }
 }
 
